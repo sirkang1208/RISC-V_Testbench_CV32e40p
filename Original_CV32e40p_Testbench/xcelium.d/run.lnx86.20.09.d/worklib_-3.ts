a56a5904dc9d0e768df94b944799a43b0937ec2e
sirkang1208 <2hiweb@naver.com>
2024-10-16 20:07:04 +0900

    modifying tbench memory

--- Original_CV32e40p_Testbench/xcelium.d/run.lnx86.20.09.d/worklib_-3.ts
+++
@@ -1,2 +0,0 @@
-1449501586 /TOOLS/CADENCE/XCELIUMMAIN2009/tools.lnx86/methodology/UVM/CDNS-1.2-ML/sv/src/uvm_pkg.sv
-1526655670 /TOOLS/CADENCE/XCELIUMMAIN2009/tools.lnx86/methodology/UVM/CDNS-1.2/additions/sv/cdns_uvm_pkg.sv
